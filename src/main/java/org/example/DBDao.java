2095b5fac2ecf2484715a6ba79776b2905c0906b
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-05-16 10:41:22 +1000

    Data loader now loads purchase price as int

--- src/main/java/org/example/DBDao.java
+++ src/main/java/org/example/DBDao.java
@@ -12,14 +12,30 @@ import org.bson.Document;
 public class DBDao {    
     public void createResidence(List<Document> newResidences) {
         // Establish connection
-        try (MongoClient mongoClient = MongoClients.create("mongodb+srv://shirozianh:lReVvB53gWWFTOyx@realestatedata.ncrbvt4.mongodb.net/")) {
+        // try (MongoClient mongoClient = MongoClients.create("mongodb+srv://shirozianh:lReVvB53gWWFTOyx@realestatedata.ncrbvt4.mongodb.net/")) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017/")) {
             // Get the database
             MongoDatabase sampleTrainingDB = mongoClient.getDatabase("RealEstateDB");
             
             // Get the specific collection of residencies
             MongoCollection<Document> residenciesCollection = sampleTrainingDB.getCollection("residencies");
 
-            // Add new residence
+
+            // Convert purchase_price to int
+            for (Document doc : newResidences) {
+                Object priceObj = doc.get("purchase_price");
+                if (priceObj instanceof String) {
+                    try {
+                        int price = Integer.parseInt((String) priceObj);
+                        doc.put("purchase_price", price);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Invalid purchase_price: " + priceObj);
+                    }
+                }
+            }
+
+
+            // Add new residence, 
             residenciesCollection.insertMany(newResidences);
         } catch (Exception e) {
             System.err.println("Error in createResidence Dao: " + e.getMessage());
